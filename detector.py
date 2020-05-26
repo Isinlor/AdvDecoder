8456415959decfc5124b4c0eaac1b235fb657c9e
Tomasz Darmetko <isinlor@gmail.com>
2020-05-26 12:04:03 +0200

    Add changes.

--- detector.py
+++ detector.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer, GPT2Tokenizer, GPT2LMHeadModel, RobertaForSequenceClassification
 
-from AdvDecoder import decode
+from AdvDecoder import decode, batch_decode
 from BatchTextGenerationPipeline import BatchTextGenerationPipeline
 from IsFakePipeline import IsFakePipeline
 
@@ -13,4 +13,8 @@ detector_model = RobertaForSequenceClassification.from_pretrained("roberta-base-
 classifier = IsFakePipeline(model=detector_model, tokenizer=detector_tokenizer)
 generator = BatchTextGenerationPipeline(model=model, tokenizer=tokenizer)
 
-print(decode(prompt="", generate_length=50, step=10, sequences=3, generator=generator, classifier=classifier))
+print("\n\n ============== Top-K Decoder\n\n")
+print("\n--------------------\n".join(generator.generate("", top_k=150, generate_length=100, num_return_sequences=10, do_sample=True, no_repeat_ngram_size=3)))
+
+print("\n\n ============== Adversarial Decoder\n\n")
+print("\n--------------------\n".join(batch_decode(10, prompt="", generate_length=100, step=10, sequences=10, generator=generator, classifier=classifier)))
